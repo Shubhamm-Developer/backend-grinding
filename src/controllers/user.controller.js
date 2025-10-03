f3b2b61963d28e15ea786612f659f1d15968e0a5
Shubhamm-Developer <shivamfortyseven#gmail.com>
2025-10-03 23:53:09 +0530

    adding user functionality to update passoword , and to change username , coverImage or avatar , and fixed some typos

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -225,18 +225,138 @@ const refreshAccessToken = asyncHandler(async (req,res)=> {
      .cookie("refrestToken", newRefreshToken, options)
      .json(
          new ApiResponse(
-             w00,
+             200,
              {accessToken, refreshToken: newRefreshToken},
              "access token refreshed!"
          )
      )
    } catch (error) {
     throw new ApiError(401 , error?.message || " invalid refresh token")
-    
    }
+    
+   });
 
+const changeCurrentPassword = asyncHandler(async(req,res)  => {
 
+    const {oldPassword , newPassowrd} = req.body
+
+    const user = await User.findById(req.user?._id)
+    const isPasswordCorrect = await user.isPasswordCorrect(oldPassword)
+
+    if (!isPasswordCorrect) {
+        throw new ApiError(400 , "Invalid old passoword"
+        )
+        
+    }
+
+    user.password = newPassowrd
+    await user.save({validateBeforeSave:false})
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200 , {},
+        "password changed successfully"
+    ))
 })
+
+const getCurrentUser = asyncHandler(async(req,res) =>{
+    return res
+    .status(200)
+    .json(200, req.user, "current user fetched successfully")
+    })
+
+
+const updateAccountDetails = asyncHandler(async(req,res)=> {
+    const {fullname , email} = req.body
+
+    if (!fullname && !email) {
+        throw new ApiError (400 ,"both fields are required")
+        
+    }
+
+    const user = User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set:{
+                fullname,
+                email
+            }
+        },
+        {new: true}
+    ).select("-password")
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, user , "Accounts details updated successfully !"
+    ))
+})
+
+
+const updateUserAvatar = asyncHandler(async(req,res) =>{
+    const avatarLocalPath = await req.file?.path
+
+    if (!avatarLocalPath) {
+        throw new ApiError(400,"Avatar file is missing")
+    }
+    const avatar = await uploadOnCloudinary(avatarLocalPath)
+
+    if(!avatar.url){
+        throw new ApiError(400, "error while uploading on avatar")
+    }
+
+    const user = await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set: {
+                avatar: avatar.url
+            }
+        },
+        {
+            new: true
+        }
+    ).select("-password")
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse (200 , user , " avatar updated successfully "
+        )
+    )
+})
+
+
+const updateUserCoverImage = asyncHandler(async(req,res) =>{
+    const coverImageLocalPath = await req.file?.path
+
+    if (!coverImageLocalPath) {
+        throw new ApiError(400,"cover image file is missing")
+    }
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+    if(!coverImacoverImage.url){
+        throw new ApiError(400, "error while uploading on coverImacoverImage")
+    }
+
+    const user = await await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set: {
+                coverImage: coverImage.url
+            }
+        },
+        {
+            new: true
+        }
+    ).select("-password")
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse (200 , user , " coverImage updated successfully "
+        )
+    )
+})
+
 
 
 
@@ -244,5 +364,10 @@ const refreshAccessToken = asyncHandler(async (req,res)=> {
 export { registerUser,
     loginUser,
     logoutUser,
-    refreshAccessToken
+    refreshAccessToken,
+    changeCurrentPassword,
+    getCurrentUser,
+    updateAccountDetails,
+    updateUserAvatar,
+    updateUserCoverImage
  }
---
+++ src/models/subscriptions.model.js
@@ -0,0 +1,25 @@
+import mongoose, { Schema } from "mongoose";
+
+const subscriptionSchema = new Schema({
+
+    subscriber: {
+        type: Schema.Types.ObjectId,
+        // the one who is subscribing 
+        ref: "User"
+
+    },
+    channel: {
+        type: Schema.Types.ObjectId,
+        // the one to whom subscrber is subscribing
+        ref: "User"
+    }
+},{timestamps: true}
+)
+
+
+
+
+
+
+
+export const Subscription = mongoose.model("Subscription", subscriptionSchema)
